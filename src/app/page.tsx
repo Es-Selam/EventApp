c651fe605135b2c054d20b9bd77139c557e7648c
asipiome <asipiome@students.zhaw.ch>
2024-02-16 20:59:16 +0100

    new approach with grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,19 @@ import React from 'react';
 import Carousel from "@/app/Carousel";
 
 export default function Home() {
-  return (
-  <main className="flex flex-col items-center justify-center min-h-screen p-4">
-    <h1 className="text-4xl font-bold mb-8">Anstehende Events</h1>
-    <Carousel />
-  </main>
-  );
+    return (
+        <main className="grid grid-cols-3 grid-rows-6 min-h-screen
+        max-h-screen border-4 border-amber-400">
+            <div className="flex items-center justify-center border-4 col-span-full border-red-400 text-center">1</div>
+            <div
+                className="flex items-center justify-center border-4 col-span-full row-span-4 border-red-400 text-center">2
+            </div>
+            <div className="flex items-center justify-center border-4 border-red-400 text-center">3</div>
+            <div className="flex items-center justify-center border-4 border-red-400 text-center">4</div>
+            <div className="flex items-center justify-center border-4 border-red-400 text-center">5</div>
+
+            {/*<h1 className="text-4xl font-bold">Welcome to Es-Selam Events!</h1>*/}
+            {/*<Carousel />*/}
+        </main>
+    );
 }
