40d3a8e6cdc180c31b861d3402d6e27c2d7609ef
asipiome <asipiome@students.zhaw.ch>
2024-02-17 21:08:54 +0100

    -implemented dynamic font size for title to prevent overlapping when too big titles are written

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,13 +84,45 @@ const Home = () => {
         return () => window.removeEventListener('resize', updateDynamicHeight);
     }, []);
 
-    const currentEvent = events[currentEventIndex];
+    const useDynamicFontSize = (title:string) => {
+        const [dynamicFontSize, setDynamicFontSize] = useState('1rem');
 
+        useEffect(() => {
+            const updateFontSize = () => {
+                // Constants to define your scaling
+                const maxWidthForBaseSize = 500; // max window width to apply the base font size
+                const baseSize = 80; // base font size for the smallest window width
+                const scalingFactor = 0.7; // adjust this factor to control scaling
+
+                // Calculate the font size based on the window width and title length
+                let size = Math.min(window.innerWidth / maxWidthForBaseSize, 1) * (baseSize - title.length) * scalingFactor;
+
+                // Set a minimum font size
+                size = Math.max(size, 16);
+
+                setDynamicFontSize(`${size}px`);
+            };
+
+            // Initial update
+            updateFontSize();
+
+            // Add event listener to update font size when the window is resized
+            window.addEventListener('resize', updateFontSize);
+
+            // Cleanup event listener
+            return () => window.removeEventListener('resize', updateFontSize);
+        }, [title]);
+
+        return dynamicFontSize;
+    };
+
+    const currentEvent = events[currentEventIndex];
+    const titleFontSize = useDynamicFontSize(currentEvent?.title || '');
     return (
         <main style={{ height: dynamicHeight }} className={`grid grid-cols-3 grid-rows-6 dark:text-[#ececec]`}>
             {currentEvent ? (
                 <>
-                    <div className="flex items-center justify-center col-span-full text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-extrabold tracking-wide border-b-8 border-black dark:border-white">
+                    <div style={{ fontSize: titleFontSize }} className="flex items-center justify-center col-span-full text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-extrabold tracking-wide border-b-8 border-black dark:border-white">
                         {currentEvent.title}
                     </div>
                     <div className="px-20 my-auto col-span-full row-span-4
